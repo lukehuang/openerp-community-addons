80e0ab9de97c11fa0d6d7c86f7d7b175663ede9a
oldrev <oldrev@4340ef2c-ba08-11dd-8622-756dacf128ff>
2009-03-03 06:25:31 +0000

    添加了中国会计科目

---
+++ trunk/addons/l10n_chart_cn/__terp__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+{
+    "name" : "中国会计科目",
+    "version" : "1.0",
+    "author" : "xinglv@hotmail.com",
+    "website": "http://www.shine-it.net",
+    "category" : "Localisation/Account charts",
+    "depends" : ["account", "account_chart"],
+    "demo_xml" : [],
+    "update_xml" : ["account_chart.xml"],
+    "active": False,
+    "installable": True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+

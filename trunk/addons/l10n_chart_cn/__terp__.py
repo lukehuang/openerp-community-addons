ccc95a9faa82091832b2fa05a37570e258165f87
digisatori <digisatori@4340ef2c-ba08-11dd-8622-756dacf128ff>
2009-05-06 06:15:51 +0000

    adapt the account chart module to openERP V5's format

--- trunk/addons/l10n_chart_cn/__terp__.py
+++ trunk/addons/l10n_chart_cn/__terp__.py
@@ -1,8 +1,17 @@
 # -*- encoding: utf-8 -*-
 {
     "name" : "中国会计科目",
-    "version" : "1.0",
+    "version" : "2.0",
     "author" : "xinglv@hotmail.com",
+    'description': """
+    中国会计科目模板，用于生成会计科目表
+    用法：安装此模块后,至“财务管理>>设置>>财务会计>>模板“双击”从模板
+    生成会计科目表“，点击”创建“即。
+
+    鸣谢：
+    wjfonand 提供会计科目表csv文件
+    xinglv   将csv文件转换为符合V4版本格式的xml文件
+    """,
     "website": "http://www.shine-it.net",
     "category" : "Localisation/Account charts",
     "depends" : ["account", "account_chart"],

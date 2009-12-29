27af8e80755fb6375a31f2c4a3009271c4bfb23e
digisatori <digisatori@4340ef2c-ba08-11dd-8622-756dacf128ff>
2009-12-29 08:35:06 +0000

    modify base_report_cn to corret  "redefining named object" error and KeyError: SimHeiOblique

--- trunk/addons/base_report_cn/__init__.py
+++ trunk/addons/base_report_cn/__init__.py
@@ -1,19 +1,20 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2009 Gábor Dukai
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
@@ -71,19 +72,14 @@ def wrap_trml2pdf(method):
             endtag = ''
         data = odata[:i] + starttag
         adp = os.path.abspath(config['addons_path'])
-        for new in fontmap.itervalues():
+        for new in set(fontmap.values()):
             fntp = os.path.normcase(os.path.join(adp, 'base_report_cn', 'fonts', new))
             data += '    <registerFont fontName="' + new + '" fontFile="' + fntp + '.ttf"/>\n'
         data += endtag + odata[i:]
-        for  old, new in fontmap.iteritems():
-            data = data.replace(old, new)
-
-        if len(args) > 2:
-            myargs = args[1:]
-        else:
-            myargs = args[1]
-            
-        return method(data, myargs, **argv)
+        while len(fontmap)>0:
+            ck=max(fontmap)
+            data = data.replace(ck,fontmap.pop(ck))
+        return method(data, args[1:] if len(args) > 2 else args[1], **argv)
     return convert2TrueType
 
 report.render.rml2pdf.parseString = wrap_trml2pdf(report.render.rml2pdf.parseString)

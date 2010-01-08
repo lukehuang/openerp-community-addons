9d795a42a23bc92010fce428fd02dfeae781a97a
oldrev <oldrev@4340ef2c-ba08-11dd-8622-756dacf128ff>
2010-01-08 05:12:13 +0000

    细节调整

--- addons/l10n_cn/__terp__.py
+++ addons/l10n_cn/__terp__.py
@@ -18,19 +18,22 @@
 #
 ##############################################################################
 {
-    "name" : "Chinese Unicode Reports with TrueType fonts",
+    "name" : "OpenERP 基础中文化模块",
     "version" : "1.1",
     "author" : "oldrev/digitalsatori",
     "description": """
-    此模块用于为 OpenERP 的基础系统提供汉化，仅依赖内置的 base 模块。\n\n
+    此模块用于为 OpenERP 的基础系统提供汉化，仅依赖内置的 base 模块。
     
-    此模块包含如下功能：\n
-    1.  为 OpenERP 的 RML/PDF 报表提供中文字体支持\n
-    2.  为 OpenERP 添加中文省份数据\n
-    \n
+    此模块包含如下功能：
+    1.  为 OpenERP 的 RML/PDF 报表提供中文字体支持
+    2.  为 OpenERP 添加中文省份数据
+    3.  本地化国家数据。
+    4.  本地化货币数据，并设置人民币为本币而不是默认的欧元。
 
-    使用文泉驿正黑体和 AR PL SungtiL GB 宋体替换系统原来不支持 Unicode 的英文字体，安装此模块可以自动使内置的报表自动支持中文字体。 \n
-    NOTE: 直接解压复制到 addons 目录. 不用导入!\n
+    中文说明：
+    使用文泉驿正黑体和 AR PL SungtiL GB 宋体（二者均为开源字体）替换系统原来不支持 Unicode 的英文字体，安装此模块可以使内置的报表自动支持中文字体。 
+    注意: 直接解压复制到 addons 目录. 然后更新模块列表并安装，此模块不能用导入功能安装。如果您使用的是 Windows 系统的话推荐使用 Windows 的 SimHei.ttf 和 SimSun.ttf 替换 fonts 目录中的相应字体文件。
+
     原始 base_report_unicode 模块作者：Gábor Dukai
         
     """,
